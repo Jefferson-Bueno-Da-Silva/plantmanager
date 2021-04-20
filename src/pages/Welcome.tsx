16b82c47089cf67c683416147a02b0fe5e4bbc22
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-20 18:43:03 -0300

    fontFamily, layout and icons

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,7 +6,11 @@ import {
   TouchableOpacity, 
   StyleSheet,
   Dimensions,
+  View,
 } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
+import fonts from '../styles/fonts';
 
 import wateringImg from "../assets/watering.png";
 import colors from '../styles/colors';
@@ -14,33 +18,35 @@ import colors from '../styles/colors';
 export function Welcome(){
   return(
     <SafeAreaView style={styles.container}>
-      
-      <Text style={ styles.title }>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
-      
-      <Image 
-        source={wateringImg} 
-        style={ styles.image }
-        resizeMode="contain"
-      />
-
-      <Text style={ styles.subtitle }>
-        Não esqueça mais de regar suas plantas.
-        Nós Cuidamos de lembrar você sempre que precisar.
-      </Text>
-
-      <TouchableOpacity 
-        style={ styles.button }
-        activeOpacity={0.7}
-      >
-        <Text style={styles.buttonText} >
-          =
+      <View style={styles.wrapper} >
+        <Text style={ styles.title }>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
         </Text>
-      </TouchableOpacity>
+        
+        <Image 
+          source={wateringImg} 
+          style={ styles.image }
+          resizeMode="contain"
+        />
 
+        <Text style={ styles.subtitle }>
+          Não esqueça mais de regar suas plantas.
+          Nós Cuidamos de lembrar você sempre que precisar.
+        </Text>
+
+        <TouchableOpacity 
+          style={ styles.button }
+          activeOpacity={0.7}
+        >        
+          <Feather
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -48,21 +54,28 @@ export function Welcome(){
 const styles = StyleSheet.create({
   container :{
     flex:1,
+  },
+  wrapper:{
+    flex:1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 30
   },
   title :{
-    fontSize:32,
+    fontSize:28,
     fontWeight: 'bold',
     textAlign: 'center',
     color : colors.heading,
-    marginTop:38
+    marginTop:38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle :{
     textAlign:'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image :{
     height: Dimensions.get('window').width * 0.7,
@@ -76,8 +89,8 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56,
   },
-  buttonText :{
+  buttonIcon :{
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24
   },
 })
